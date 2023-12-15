61f75cc6896b94cd90f357c288a2e158541beb47
FranksRobins <francis.f.robinson@gmail.com>
2023-12-15 03:41:28 -0500

    terraform vars fix

--- terraform/providers/github/service-template/terraform_module/main.tf
+++ terraform/providers/github/service-template/terraform_module/main.tf
@@ -45,7 +45,7 @@ resource "github_repository_file" "ci_file" {
 // Register service manifest in Consul
 resource "consul_keys" "manifest_key" {
   key {
-    path   = "${var.root_dir}/${var.repo_name}"
+    path   = "${var.consul_path}/${var.repo_name}"
     value  = jsonencode({
         "service-id": var.service_id,
         "repo-provider": "github",
