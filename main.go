b0785a1c24d1862bc2a772bc6819876769e7f95c
FranksRobins <francis.f.robinson@gmail.com>
2024-02-02 13:09:40 -0500

    adding comments to main.go

--- main.go
+++ main.go
@@ -8,22 +8,26 @@ import (
 )
 
 func main() {
+	// Load main configuration file
 	err := config.LoadConfiguration()
 	if err != nil {
 			l.Fatal("Failed to load configuration: %v", err)
 	}
 
+	// Initialize server metrics
   err = stats.InitMetrics()
   if err != nil {
 			l.Fatal("Failed to initialize metrics service: %v", err)
   }
 
+	// Initialize logging system
 	err = l.InitLogger(config.RootDir + "/" + config.AppConfig.LogDir)
   if err != nil {
 			l.Fatal("Error initializing logs: %v", err)
   }
   defer l.CloseLogger()
 
+	// Run CLI
   rootCmd := cli.SetupRootCommand()
   if err := rootCmd.Execute(); err != nil {
 			l.Fatal("Error executing command: %v", err)
