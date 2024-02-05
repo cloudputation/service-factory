22499d4e743be460fdded1882ef6a6081d3a9276
FranksRobins <francis.f.robinson@gmail.com>
2024-02-05 17:30:03 -0500

    ci tests

--- helpers/create_release.sh
+++ helpers/create_release.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 RELEASE_TAG=$(cat VERSION)
+GITHUB_SF_TOKEN="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"
+GITHUB_REMOTE_URL="https://${GITHUB_SF_TOKEN}@github.com/cloudputation/service-factory.git"
+GITLAB_REMOTE_URL="git@gitlab.com:cloudputation/service-factory.git"
+git remote remove origin
+git remote add origin $GITHUB_REMOTE_URL
 git tag -a v${RELEASE_TAG} -m "Creating release for version: ${RELEASE_TAG}"
 git push origin v${RELEASE_TAG}
+git remote remove origin
+git remote add origin $GITLAB_REMOTE_URL
